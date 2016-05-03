aba5a54aa6f0e8ed69d95414ca8d83d61f0f35fe
Mike Veldsink <veldsinkie.mv@gmail.com>
2016-05-03 19:55:58 +0200

    first commit

---
+++ osu!NET/OsuAPI.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using osu.objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu
+{
+    public class OsuAPI
+    {
+        OsuCredentials credentials;
+
+        public OsuAPI(string key)
+        {
+            credentials = new OsuCredentials(key);
+        }
+
+        /// <summary>
+        /// Get a user based on the user's name.
+        /// </summary>
+        /// <param name="name">user's name</param>
+        /// <returns></returns>
+        public User GetUser(string name)
+        {
+            string data = DownloadData("api/get_user?&k=" + credentials.GetKey() + "&u=" + name);
+            if (data != "")
+            {
+                List<User> d = JsonConvert.DeserializeObject<List<User>>(data);
+                return d[0];
+            }
+            return new User();
+        }
+
+        /// <summary>
+        /// Get a user based on the user's id.
+        /// </summary>
+        /// <param name="id">user's id</param>
+        /// <returns></returns>
+        public User GetUser(long id)
+        {
+            string data = DownloadData("api/get_user?&k=" + credentials.GetKey() + "&u=" + id);
+            if (data != "")
+            {
+                List<User> d = JsonConvert.DeserializeObject<List<User>>(data);
+                return d[0];
+            }
+            return new User();
+        }
+
+        private string DownloadData(string url)
+        {
+            WebClient c = new WebClient();
+
+            byte[] b;
+
+            b = c.DownloadData("https://osu.ppy.sh/" + url);
+            if (b != null)
+            {
+                string result = Encoding.UTF8.GetString(b);
+                return result;
+            }
+            return "";
+        }
+    }
+}
---
+++ osu!NET/objects/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.objects
+{
+    public class User
+    {
+        public string user_id;
+        public string username;
+        public string count300;
+        public string count100;
+        public string count50;
+        public string playcount;
+        public string ranked_score;
+        public string total_score;
+        public string pp_rank;
+        public string level;
+        public string pp_raw;
+        public string accuracy;
+        public string count_rank_ss;
+        public string count_rank_s;
+        public string count_rank_a;
+        public string country;
+        public string pp_country_rank;
+        public List<string> events;
+    }
+}
